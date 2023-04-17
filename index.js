4fbc2a8ed5a8f45bb52ef31da1c56569fe7784af
mssjew <86316072+mssjew@users.noreply.github.com>
2023-04-17 19:04:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ if (currentDay === 0 || currentDay === 6) {
     let socket = new WebSocket("wss://marketdata.tradermade.com/feedadv");
   
     socket.onopen = function (e) {
-      socket.send('{"userKey":"wsLwWGUJ4VGYzCjj249w", "symbol":"XAUUSD"}');
+      socket.send('{"userKey":"ws545XMhp4v7VPgU4pRg", "symbol":"XAUUSD"}');
       
     };
   
