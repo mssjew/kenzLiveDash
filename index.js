66864f64cd4c6a90f0561ad600318fadbc1d5471
hmzsjd <github@hamzasajid.com>
2023-06-12 10:36:50 +0300

    .

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ if (currentDay === 0 || currentDay === 6) {
     let socket = new WebSocket("wss://marketdata.tradermade.com/feedadv");
   
     socket.onopen = function (e) {
-      socket.send('{"userKey":"wsfpmWkr6KZmvpXqJ6TA", "symbol":"XAUUSD"}');
+      socket.send('{"userKey":"wsAu2r68f7Vtgl1grS4Q", "symbol":"XAUUSD"}');
       
     };
   
